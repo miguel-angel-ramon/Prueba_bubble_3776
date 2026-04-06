04c1240417093b674e1606b010203b2f64b2fb6b
Miguel A. <miguel.angel.ramon@ayesa.com>
2026-04-06 23:46:04 +0200

    prueba 1874

--- src/main/java/com/example/ComplexS3776Demo.java
+++ src/main/java/com/example/ComplexS3776Demo.java
@@ -1,65 +1,26 @@
 package com.example;
 
-import java.util.function.Function;
-
 public class ComplexS3776Demo {
 
     /**
      * @deprecated use newApi instead
      */
     @Deprecated
-    public void oldApiWithReplacement() {
-        System.out.println("old api with replacement");
+    public void oldApi() {
+        System.out.println("old api");
     }
 
     public void newApi() {
         System.out.println("new api");
     }
 
-    @Deprecated
-    public void oldApiWithoutReplacement() {
-        System.out.println("old api without replacement");
-    }
-
-    @SuppressWarnings("java:S1874")
-    public void testAll() {
-        Thread t = new Thread();
-        t.stop();
-
-        oldApiWithReplacement();
-        oldApiWithoutReplacement();
-    }
-
-    public void processLambdas() {
-        Function<String, String> f1 = s -> {
-            if (s.isEmpty()) {
-                return "EMPTY";
-            }
-            return s.toUpperCase();
-        };
-
-        Function<String, String> f2 = s -> {
-            if (s.length() > 3) {
-                return "LONG";
-            }
-            return "SHORT";
-        };
-
-        Function<String, String> f3 = s -> {
-            if (s.startsWith("A")) {
-                return "A";
-            }
-            return "OTHER";
-        };
-
-        System.out.println(f1.apply("abc"));
-        System.out.println(f2.apply("abcd"));
-        System.out.println(f3.apply("Apple"));
+    public void test() {
+        // Uso de método deprecated → dispara S1874
+        oldApi();
     }
 
     public static void main(String[] args) {
         ComplexS3776Demo app = new ComplexS3776Demo();
-        app.testAll();
-        app.processLambdas();
+        app.test();
     }
 }
